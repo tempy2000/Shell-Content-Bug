7d67835b2256f808a103b133d6f49365f9a228a0
Matthew Templeton <47399002+tempy2000@users.noreply.github.com>
2023-01-10 12:10:43 +0000

    Add files via upload

---
+++ AppShell.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace MauiApp1;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+
+    async void SettingsClicked(object sender, EventArgs e)
+    {
+
+    }
+    async void QuitClicked(object sender, EventArgs e)
+    {
+
+    }
+}
